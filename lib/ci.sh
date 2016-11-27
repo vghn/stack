6c81c96bb354b46b832793a9d3dcdc82216e945b
Vlad <vlad@ghn.me>
2016-11-27 08:33:20 -0600

    Load VGS Library during installation

--- lib/ci.sh
+++ lib/ci.sh
@@ -6,6 +6,10 @@ ci_install(){
   echo 'Install VGS library'
   git clone https://github.com/vghn/vgs.git ~/vgs
 
+  # Load VGS library (https://github.com/vghn/vgs)
+  # shellcheck disable=1090
+  . ~/vgs/load 2>/dev/null || true
+
   echo 'Install AWS-CLI'
   pip install --user --upgrade awscli
 
