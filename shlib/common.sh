c88ae3cb3a3c782c606d74a2844348db3c120fd4
Vlad <vlad@ghn.me>
2017-09-03 15:48:41 -0500

    Add .env encrypt/decrypt commands
    Closes #104

--- shlib/common.sh
+++ shlib/common.sh
@@ -101,12 +101,21 @@ validate_amis(){
   eval "${APPDIR}/bin/ami" validate
 }
 
+# Encrypt .env file
+encrypt_env(){
+  if [[ -s "${APPDIR}/.env" ]]; then
+    e_info 'Encrypt .env'
+    ( echo "$ENCRYPT_KEY" | base64 --decode ) | \
+      gpg --batch --yes --symmetric --passphrase-fd 0 --cipher-algo AES256 --s2k-digest-algo SHA512 --output "${APPDIR}/.env.gpg" "${APPDIR}/.env"
+  fi
+}
+
 # Decrypt .env file
 decrypt_env(){
   if [[ ! -s "${APPDIR}/.env" ]]; then
     e_info 'Decrypt .env'
     ( echo "$ENCRYPT_KEY" | base64 --decode ) | \
-      gpg --decrypt --passphrase-fd 0 --batch --output "${APPDIR}/.env" "${APPDIR}/.env.gpg"
+      gpg --batch --yes --decrypt --passphrase-fd 0 --output "${APPDIR}/.env" "${APPDIR}/.env.gpg"
   fi
 }
 
